8801ee5d5504e270c4eb412497d7e18d16f37d57
patriciaSR <patricia.suarez.psr@gmail.com>
2020-01-08 11:35:58 +0100

    add https certificate config

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 
 const repository = require('./repository/');
 
@@ -16,6 +18,14 @@ app.use(express.json());
 app.use('/posts', postsRouter);
 app.use('/posts/:id/comments', commentsRouter);
 app.use('/offensive-words', offensiveRouter);
+
+// openssl req -nodes -new -x509 -keyout server.key -out server.cert es la forma de generar el certificado de mentira
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert'),
+}, app).listen(3443, () => {
+  console.log("Https server started in port 3443");
+});
 
 async function main() {
   await repository.connect();
