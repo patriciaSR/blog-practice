f4305ca99dc551bcf06a3b05ab64b3d9f0e69b7d
patriciaSR <patricia.suarez.psr@gmail.com>
2020-01-09 17:31:35 +0100

    add another reg exp but doesnt work

--- src/js/validator.js
+++ src/js/validator.js
@@ -3,6 +3,7 @@ function haveOffensiveWords(text, offensiveWords) {
   const notAllowedOffensiveWords = [];
 
   offensiveWords.forEach((word) => {
+    // const isWordIncluded = (/\b(word.word)\b/gm).test(text.toLowerCase());
     const isWordIncluded = new RegExp("\\b" + word.word + "\\b").test(text.toLowerCase());
 
     if (isWordIncluded) {
