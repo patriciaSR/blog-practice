5a1e351e06096be097fe47f5080998ddfd5464fc
patriciaSR <patricia.suarez.psr@gmail.com>
2020-01-21 11:27:31 +0100

    remove https dependencie from front

--- client/src/resources/loadPosts.js
+++ client/src/resources/loadPosts.js
@@ -1,15 +1,9 @@
 import * as axios from 'axios'
-const https = require('https')
-
 
 const ENDPOINT = 'https://localhost:3443/'
 
-const agent = new https.Agent({
-  rejectUnauthorized: false
-})
-
 async function loadPosts() {
-  const result = await axios.get(ENDPOINT + 'posts', {httpsAgent: agent})
+  const result = await axios.get(ENDPOINT + 'posts')
 
   let posts = []
   for (let post of result.data) {
