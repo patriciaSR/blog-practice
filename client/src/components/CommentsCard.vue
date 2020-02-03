3c3b19370704a1dfc6a3391299edac43408572d8
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-03 10:53:28 +0100

    add Dialog component

--- client/src/components/CommentsCard.vue
+++ client/src/components/CommentsCard.vue
@@ -51,35 +51,7 @@
       </v-card-actions>
     </v-card>
 
-    <v-dialog v-if="offensiveError" v-model="dialog" max-width="500">
-      <v-card v-if="offensiveError" color="#FAD9D3" class="pa-4">
-        <v-card-text class="text--primary" data-id="error-text">{{offensiveError.errorText}}</v-card-text>
-        <v-list color="#FAD9D3">
-          <v-list-item
-            v-for="word in offensiveError.notAllowedWords"
-            :key="word.word"
-            three-line
-            class="d-block pb-3 mb-3 white"
-          >
-            <v-list-item-content shaped>
-              <v-list-item-title class="mb-2">
-                <strong>Word:</strong>
-                {{word.word}}
-              </v-list-item-title>
-              <v-list-item-subtitle class="caption">
-                <strong>Level:</strong>
-                {{word.level}}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <Dialog :offensiveError="offensiveError" :dialog="dialog" @close-dialog="dialog = false"/>
   </v-card>
 </template>
 
@@ -94,13 +66,16 @@ import deleteComment from '../resources/deleteComment'
 import AvatarCard from '../components/AvatarCard'
 import PrimaryBtn from '../components/Btns/PrimaryBtn'
 import SecondaryBtn from '../components/Btns/SecondaryBtn'
+import Dialog from '../components/Dialog'
+
 
 export default {
   name: 'CommentsCard',
   components: {
     AvatarCard,
     PrimaryBtn,
-    SecondaryBtn
+    SecondaryBtn,
+    Dialog
   },
   props: {
     postID: undefined,
---
+++ client/src/components/Dialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-dialog v-if="offensiveError" v-model="dialog" max-width="500">
+    <v-card v-if="offensiveError" color="#FAD9D3" class="pa-4">
+      <v-card-text class="text--primary" data-id="error-text">{{offensiveError.errorText}}</v-card-text>
+      <v-list color="#FAD9D3">
+        <v-list-item
+          v-for="word in offensiveError.notAllowedWords"
+          :key="word.word"
+          three-line
+          class="d-block pb-3 mb-3 white"
+        >
+          <v-list-item-content shaped>
+            <v-list-item-title class="mb-2">
+              <strong>Word:</strong>
+              {{word.word}}
+            </v-list-item-title>
+            <v-list-item-subtitle class="caption">
+              <strong>Level:</strong>
+              {{word.level}}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="closeDialog()">Agree</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'Dialog',
+  components: {},
+  props: {
+    offensiveError: undefined,
+    dialog: undefined
+  },
+  data: () => ({}),
+  methods: {
+     closeDialog() {
+      this.$emit('close-dialog')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
