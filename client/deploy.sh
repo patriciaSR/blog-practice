c893b2cb1c54cd606a0dfadcc6435604cefd1bf0
patriciaSR <patricia.suarez.psr@gmail.com>
2020-02-05 10:31:48 +0100

    add dist folder

--- client/deploy.sh
+++ client/deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:patriciaSR/blog-practice.git master:gh-pages
+git push -f master:gh-pages
 
 cd -
